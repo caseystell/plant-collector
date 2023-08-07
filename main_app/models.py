7895227e7a5ab4410e9a9f462539ad986c8bcdeb
caseystell <cleigh14@hotmail.com>
2023-08-07 08:49:19 -0600

    create detail page and update database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=150)
+    type = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    bears_fruit = models.BooleanField()
+    flowers = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-
-plants = [
-    {'name': 'Prickly Pear', 'type': 'Cactus', 'description': 'Paddle-shaped leaves grow in a cluster formation. Caution: very spiky!', 'bears_fruit': True, 'flowers': True},
-    {'name': 'Desert Willow', 'type': 'Tree', 'description': 'Long thin green leaves and a wide-spread shape', 'bears_fruit': False, 'flowers': True},
-    {'name': 'Ice Plant', 'type': 'Succulent', 'description': 'Finger-shaped leaves; makes for an excellent ground cover and requires little water', 'bears_fruit': False, 'flowers': True},
-    {'name': 'Ocotillo', 'type': 'Cactus', 'description': 'Long spiky arms reach up toward the sky; grows beautiful green leaves with lots of rain!', 'bears_fruit': False, 'flowers': True},
-    {'name': 'Peach Tree', 'type': 'Tree', 'description': 'Brown bark with banana-shaped leaves; grows fairly tall', 'bears_fruit': True, 'flowers': True},
-]
+from .models import Plant
 
 def home(request):
   return render(request, 'home.html')
@@ -15,6 +8,11 @@ def about(request):
   return render(request, 'about.html')
 
 def plants_index(request):
+  plants = Plant.objects.all()
   return render(request, 'plants/index.html', {
     'plants': plants,
   })
+
+def plants_detail(request, plant_id):
+  plant = Plant.objects.get(id=plant_id)
+  return render(request, 'plants/detail.html', { 'plant': plant })
